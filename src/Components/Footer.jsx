1808a3d109619758ace385413e34e608c90b62f6
Chamok Bhadra <chamokbhadra@gmail.com>
2025-10-08 20:14:00 +0600

    footer section done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,74 @@
 import React from 'react';
+import Container from './Container';
+import { Link } from 'react-router';
+import logo from "../assets/logo.png";
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className='bg-[#001931] text-white'>
+            <Container>
+                
+                <div className="footer sm:footer-horizontal   p-10">
+                    <div>
+                        <Link className='flex items-center gap-2 font-bold text-xl' to={"/"}> <img className='h-[2.5rem] w-[2.5rem]' src={logo} alt="" /> HERO.IO</Link>
+
+                        <div class=" mt-7 flex space-x-5">
+                            <Link to={"#"} class="hover:text-red-500 transition">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-5 h-5" viewBox="0 0 24 24">
+                                    <path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z" />
+                                </svg>
+
+
+                            </Link>
+
+        
+                            <Link to={"#"} class="hover:text-blue-500 transition">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-5 h-5" viewBox="0 0 24 24">
+                                        <path d="M22 12a10 10 0 1 0-11.5 9.9v-7h-2v-3h2v-2.3c0-2 1.2-3.1 3-3.1.9 0 1.8.1 1.8.1v2h-1c-1 0-1.3.6-1.3 1.2V12h2.3l-.4 3h-1.9v7A10 10 0 0 0 22 12z" />
+                                    </svg>
+                            </Link>
+
+
+                            <Link to={"#"} class="hover:text-blue-400 transition">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-5 h-5" viewBox="0 0 24 24">
+                                        <path d="M4.98 3.5C4.98 4.88 3.88 6 2.5 6S0 4.88 0 3.5 1.12 1 2.5 1 4.98 2.12 4.98 3.5zM.5 8h4V24h-4V8zm7.5 0h3.8v2.2h.1c.5-.9 1.8-1.8 3.7-1.8 4 0 4.7 2.6 4.7 6V24h-4v-7.6c0-1.8 0-4.1-2.5-4.1-2.5 0-2.9 2-2.9 4v7.7h-4V8z" />
+                                    </svg>
+                            </Link>
+
+
+                            <Link to={"#"} class="hover:text-white transition">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 1227" fill="currentColor" class="w-5 h-5">
+                                    <path d="M714.163 519.284L1160.89 0H1047.3L667.137 442.924L366.76 0H0L468.75 681.102L0 1226.64H113.589L519.875 753.462L844.375 1226.64H1200L714.137 519.284H714.163ZM568.312 691.844L525.419 630.492L154.687 80.357H319.812L611.943 502.785L654.837 564.137L1058.06 1150.28H892.938L568.312 691.844Z" />
+                                </svg>
+                            </Link>
+                        </div>
+                    </div>
+                    <div>
+                        <h6 className="text-white text-xl font-semibold">Services</h6>
+                        <Link className="link link-hover">Branding</Link>
+                        <Link className="link link-hover">Design</Link>
+                        <Link className="link link-hover">Marketing</Link>
+                        <Link className="link link-hover">Advertisement</Link>
+                    </div>
+                    <div>
+                        <h6 className="text-white text-xl font-semibold">Company</h6>
+                        <Link className="link link-hover">About us</Link>
+                        <Link className="link link-hover">Contact</Link>
+                        <Link className="link link-hover">Jobs</Link>
+                        <Link className="link link-hover">Press kit</Link>
+                    </div>
+                    <div>
+                        <h6 className="text-white text-xl font-semibold">Legal</h6>
+                        <Link className="link link-hover">Terms of use</Link>
+                        <Link className="link link-hover">Privacy policy</Link>
+                        <Link className="link link-hover">Cookie policy</Link>
+                    </div>
+                </div>
+                <footer class="text-center py-4 text-gray-500 text-sm">
+                    © 2025 HERO.IO . All rights reserved.
+                </footer>
+            </Container>
+        </footer>
     );
 };
 
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,50 +1,53 @@
 import React from 'react';
 import { Link } from 'react-router';
 import logo from "../assets/logo.png";
+import Container from './Container';
+import { FaGithub } from "react-icons/fa";
+
 
 const NavBar = () => {
     return (
-        <div className="navbar bg-base-100 shadow-sm">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+
+        <nav className='box-border border-b border-[#e9e9e9] bg-white'>
+            <Container>
+                <div className=" navbar">
+                    <div className="navbar-start">
+                        <div className="dropdown">
+                            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                            </div>
+                            <ul
+                                tabIndex={0}
+                                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                                <li> <Link className="font-semibold text-[#632EE3]" to={"/"}>Home</Link> </li>
+                                <li> <Link className="font-semibold text-[#632EE3]" to={"/application"}>Apps</Link> </li>
+                                <li> <Link className="font-semibold text-[#632EE3]" to={"/installation"}>Installation</Link> </li>
+                            </ul>
+                        </div>
+                        <Link to={"/"} className="text-color1 font-bold text-xl h-[2.5rem] flex justify-between items-center gap-2"> <img className="w-[2.5rem] h-[2.5rem]" src={logo} alt="" /> HERO.IO</Link>
                     </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
-                    </ul>
+                    <div className="navbar-center hidden lg:flex">
+                        <div className="flex gap-5 font-semibold">
+                            <Link to="/" className="transition-all duration-300 hover:hover:text-[#632EE3] border-b-2 border-transparent hover:border-[#632EE3]">Home</Link>
+                            <Link to="/application" className="transition-all duration-300 hover:hover:text-[#632EE3] border-b-2 border-transparent hover:border-[#632EE3]">Apps</Link>
+                            <Link to="/installation" className="transition-all duration-300 hover:text-[#632EE3] border-b-2 border-transparent hover:border-[#632EE3]">Installation</Link>
+                        </div>
+                    </div>
+                    <div className="navbar-end">
+                        <a
+                            href="https://github.com/chamok192"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="btn bg-gradient-to-r from-[#632EE3] to-[#9F62F2] border-none text-white hover:opacity-90"
+                        ><FaGithub />  Contribute
+                        </a>
+                    </div>
                 </div>
-                <Link to={"/"} className="text-xl h-[2.5rem] flex justify-between items-center gap-2"> <img className="w-[2.5rem] h-[2.5rem]" src={logo} alt="" /> HERO.IO</Link>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <a className="btn">Button</a>
-            </div>
-        </div>
+            </Container>
+        </nav>
+
+
+
     );
 };
 
